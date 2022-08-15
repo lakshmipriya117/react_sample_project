330178b67f7b9913ee70a95a9fe1b3f09a8cf606
lakshmipriya <lakshmipriyar117@gmail.com>
2022-08-15 18:57:33 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+//import { render, screen } from '@testing-library/react';
+//import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//test('renders learn react link', () => {
+ // render(<App />);
+  //const linkElement = screen.getByText(/learn react/i);
+  //expect(linkElement).toBeInTheDocument();
+//});
+
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+
+it('renders without crashing', () => {
+  const div = document.createElement('app');
+const root = createRoot(div); 
+root.render(<App />);
+  //const div = document.createElement('div');
+  //ReactDOM.render(<App />, div);
 });
+
+
+
